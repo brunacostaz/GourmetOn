532f6b88ab3d4e1ac8e24cf53a4f2087724ae1d8
brunacostaz <brunacstcandeias@gmail.com>
2024-10-08 20:49:04 -0300

    convertendo css para styled-components

--- src/componentes/Hero/index.js
+++ src/componentes/Hero/index.js
@@ -1,17 +1,73 @@
-import './Hero.css'
 import '../../variaveis.css'
 import BotaoIcon from '../elementos/BotaoIcon'
 import { Download } from 'lucide-react'
+import styled from 'styled-components'
+
+const ContainerHero = styled.section`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    gap: 5em;
+    padding: var(--margin-y) var(--margin-x) var(--margin-y) 0;
+`
+
+const BgImg = styled.div`
+    content: '';
+    position: relative;
+    background-color: var(--marrom-claro);
+    border-radius: 0 18px 18px 0;
+    max-width: 20%;
+    padding-left: 2em;
+    left: 0;
+
+    img {
+        filter: drop-shadow(0 12px 12px var(--color-shadow));
+    }
+`
+
+const TextosHero = styled.article`
+    padding: var(--margin-y) 1em;
+
+    h1,h2 {
+        font-weight: normal;
+        color: var(--marrom-escuro)
+    }
+
+    h1 {
+        font-size: var(--h1-mobile);
+    }
+
+    h2 {
+        font-size: var(--h2-mobile);
+    }
+
+    span {
+        color: var(--marrom-claro);
+        font-weight: bold;
+        display: inline-block;
+    }
+`
+
+const BotoesHero = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 10px;
+    padding-top: 1em;
+`
+
 
 const Hero = () => {
     return (
-        <section className='containerHero'>
-            <div className='bg-img'>
+        <ContainerHero>
+            <BgImg>
                 <picture>
                     <source srcSet={`${process.env.PUBLIC_URL}/img/img-hero.png`} media='(min-width: 1024px)'/>
                     <img loading='lazy' src={`${process.env.PUBLIC_URL}/img/img-hero-mobile.png`} alt='foto de um prato de macarronada, com um garfo levantando um pouco da comida' />
                 </picture>
-            </div>
+            </BgImg>
             <article className='textosHero'>
                 <h1><span>Sabores</span> na sua porta e <span>Receitas</span> nas suas mãos.</h1>
 
@@ -27,7 +83,7 @@ const Hero = () => {
                     </BotaoIcon>
                 </div>
             </article>
-        </section>
+        </ContainerHero>
     )
 }
 
--- src/componentes/elementos/BotaoIcon/index.js
+++ src/componentes/elementos/BotaoIcon/index.js
@@ -1,11 +1,39 @@
-import './BotaoIcon.css'
 import '../../../variaveis.css'
+import styled from 'styled-components'
+
+const Botao = styled.button`
+    display: flex;
+    align-items: center;
+    color: white;
+    gap: 8px;
+    background-color: var(--marrom-claro);
+    border: none;
+    border-radius: 18px;
+    height: auto;
+    padding: 0 20px;
+    transition: background-color 0.4s;
+
+    p {
+    font-size: 0.90em;
+    }
+
+    &:hover {
+        background-color: var(--marrom-escuro);
+        cursor: pointer;
+    }
+
+    @media screen and (min-width: 1024px) {
+        p {
+            font-size: 1.12em;
+        }
+    }
+`
 
 const BotaoIcon = (props) => {
     return (
-        <button type='button' className='botaoIcon'>
+        <Botao>
             {props.children}
-        </button>
+        </Botao>
     )
 }
 
