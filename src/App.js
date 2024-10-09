7e2012ae94644a2171734780349be993c0f43b42
Ali Andrea <99283192+AliAndrea1@users.noreply.github.com>
2024-10-09 10:20:48 -0300

    Removendo erros

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import Hero from './componentes/Hero';
-<<<<<<< Updated upstream
 import SecaoDepoimentos from './componentes/Depoimentos';
-=======
 import Sobre from './componentes/Sobre';
 import Funcionalidades from './componentes/Funcionalidades';
 
->>>>>>> Stashed changes
 function App() {
   return (
     <div className="App">
       <Hero />
-<<<<<<< Updated upstream
       <SecaoDepoimentos />
-=======
       <Sobre />
       <Funcionalidades />
->>>>>>> Stashed changes
     </div>
   );
 }
